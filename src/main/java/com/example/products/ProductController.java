1b25b7505597df21f78e2c194f48677bc49aa76e
alhxmbs <alhexmbs@gmail.com>
2025-07-31 15:07:56 -0500

    first controller}

---
+++ src/main/java/com/example/products/ProductController.java
@@ -0,0 +1,12 @@
+package com.example.products;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @GetMapping("/")
+    public String prueba(){
+        return "eoeoeo";
+    }
+}
